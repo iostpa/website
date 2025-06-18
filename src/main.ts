b38ca7297d7f300e2cf456fd47ee6d8968f06151
dragsbruh <dragsbruh@protonmail.com>
2025-06-18 22:06:04 +0530

    (remake) i didnt not liked your website so i revamped it (any% glitchless%) please acpet

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+// @ts-ignore
+function sillylilfemboy() {
+  document.getElementById("introduction")!.textContent =
+    "im a silly little femboy waifu :3";
+  document.getElementById("about")!.textContent =
+    "oh also im peak wife material :3 >~<";
+  document.getElementById("contact")!.remove();
+  document.getElementById("socials")!.remove();
+  document.getElementById("lanyard")!.remove();
+  document.getElementById("list")!.remove();
+  document.getElementById("wife")!.remove();
+}
