4c83b3d7a42247b9d2cc5c083b01dcb215b091e3
dragsbruh <dragsbruh@protonmail.com>
2025-06-18 22:30:35 +0530

    (fix) FURINA THATS ALREADY TOO MANY COMMITS

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import { resolve, join } from "path";
-import { copyFileSync } from "fs";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss()],
   build: {
     rollupOptions: {
-      plugins: [
-        {
-          name: "copy-404",
-          closeBundle() {
-            const dist = resolve(__dirname, "dist");
-            copyFileSync(
-              "404.html",
-              join(dist, "404.html")
-            );
-          },
-        },
-      ],
+      input: {
+        main: resolve(__dirname, "index.html"),
+        notfound: resolve(__dirname, "404.html"),
+      },
     },
   },
 });
