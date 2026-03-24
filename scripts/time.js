241ed23abe45c1b3a4f1310cd668de606bb04d59
iostpa <iostpa@iostpa.com>
2026-03-24 10:48:25 +0200

    Add matrix, modify time script and 404 page

--- scripts/time.js
+++ scripts/time.js
@@ -7,6 +7,6 @@ setInterval(function () {
     const d = new Date();
     let h = addZero(d.getUTCHours() + 2);
     let m = addZero(d.getUTCMinutes());
-    let s = addZero(d.getUTCSeconds());
-    const element = document.getElementById("time").innerHTML = `It is currently ${h}:${m}:${s} for me.`;
+    // let s = addZero(d.getUTCSeconds());
+    const element = document.getElementById("time").innerHTML = `It is currently <u>${h}:${m}</u> in my timezone.`;
 }, 100);
